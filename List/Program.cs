01cf719e062e067bacb5f4ccafb32073d8d0216a
HawoonShin <sinhaoon@gmail.com>
2024-07-31 23:47:42 +0900

    stact 과제 & list 과제

---
+++ List/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace List
+{
+    internal class Program
+    {
+        //인벤토리 선언
+        class Inventory
+        {
+
+        }
+
+        //아이템 선언
+        class Item
+        {
+
+        }
+
+        class Potion : Item
+        {
+        }
+        class Weapon : Item { }
+        class Armor : Item { }
+        class Accessory : Item { }
+        class Food : Item { }
+
+        static void Main(string[] args)
+        {
+            //인벤토리 리스트 선언?작성?
+            List<string> inventroy = new();
+
+            //이벤토리의 상태표시
+            Console.WriteLine($"현재 아이템이 {}개 들어있습니다");
+
+            //숫자를 입력 받는다
+            int.TryParse(Console.ReadLine(), out int input);
+            if (input == 0)
+            {
+                //0을 입력받을 경우 랜덤 획득한다
+                Console.WriteLine("아이템을 랜덤획득합니다.");
+                //랜?덤 획득?
+                inventroy.Add("랜?덤");
+            }
+            else if (input > 0 || input < 10)
+            {
+                //1~9를 누르면 아이템을 제거한다.
+                Console.WriteLine("아이템을 제거합니다.");
+                inventroy.Remove(input);
+                //리무브는 인트로는 받을 수 없는건가요???
+            }
+            else
+            {
+                Console.WriteLine("잘못입력하셨습니다.");
+            }
+        }
+    }
+}
---
+++ Stact/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Stact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //과제 1
+            Stack<string> bracket = new Stack<string>();
+
+            Console.WriteLine("괄호입력해라");
+            //Console.ReadLine();
+            //한쪽 괄호를 스택으로 확인하고
+            
+            //반대쪽 괄호를 스택으로 확인하고
+           
+
+            //남으면 괄호가 완성되지 않는것이고
+            //남지 않으면 괄호가 완성된거고
+
+
+            //과제2
+
+
+            //교수님,조교님 진짜 죄송합니다 하나도 모르겟어요..ㅠㅠㅠ
+            //과제 1은 주석처럼 짜는건가? 까진 했지만 이걸 코딩으로 못 옮기겠어요
+            //과제 2는 문제 이해도 좀...어렵...
+        }
+    }
+}
